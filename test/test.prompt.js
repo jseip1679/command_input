b9cb7f49cdd6ddf5e0a162cbd3634d78612e4b89
Jake Seip <jake.seip@gmail.com>
2013-07-18 19:40:38 -0700

    prompt test WIP

--- test/test.prompt.js
+++ test/test.prompt.js
@@ -1,7 +1,6 @@
 var command_input = require('../')
   , should = require('should');
 
-
 var called = false;
 
 var cb =  function(obj){
@@ -10,8 +9,19 @@ var cb =  function(obj){
 
 command_input.prompt('All your base are belong to us:', cb);
 
+
+process.stdin.on('data',function(chunk){
+  console.log('Got Chunk', chunk);
+});
+
+process.stdin.resume();
+
+
+
 process.stdin.write("'Hello from command_input' \n \n");
+// process.stdin.setRawMode(true);
+process.stdin.setEncoding('utf8');
 
 setTimeout(function(){
       called.should.equal(true);
-},1000);
+},7000);
