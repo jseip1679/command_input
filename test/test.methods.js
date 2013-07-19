f40d671ea7a0f78eac726fcd4ab42e35502517c0
Jake Seip <jake.seip@gmail.com>
2013-07-18 17:31:56 -0700

    Added tests for public and private methods
    minor tweaks to commments

---
+++ test/test.methods.js
@@ -0,0 +1,23 @@
+/**
+ * Module dependencies.
+ */
+
+var command_input = require('../')
+  , should = require('should');
+
+//All public methods defined in the interface should exist on the exported object
+command_input.should.have.ownProperty('password');
+command_input.should.have.ownProperty('prompt');
+command_input.should.have.ownProperty('confirm');
+command_input.should.have.ownProperty('choose');
+
+//All private methods should be hidden.
+command_input.should.not.have.property('promptSingleLine');
+command_input.should.not.have.property('promptMultiLine');
+command_input.should.not.have.property('promptForRegexp');
+command_input.should.not.have.property('promptForDate');
+command_input.should.not.have.property('promptForNumber');
+
+
+
+
