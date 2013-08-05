127e2c02e548aa0c9882469e026157b62c0dad2c
Jake Seip <jake.seip@gmail.com>
2013-08-05 11:20:48 -0700

    travis-ci hangup fix wip

--- test/test.choice.js
+++ test/test.choice.js
@@ -13,6 +13,7 @@ child.stdout.on('data', function (data) {
 });
 
 child.stdin.end('1\n\n');
+child.stdin.destroy();
 
 child.stderr.on('data', function(data){
   throw new Error(data);
@@ -22,7 +23,7 @@ child.on('exit', function(){
   output.should.equal('  1) tobi\n  2) loki\n  3) jane\n  4) manny\n  5) luna\n  : you chose 1 "tobi"\n');
 });
 
-//Adding force kill for travis-CI 
+//Adding force kill for travis-CI
 setTimeout(function(){
   child.kill();
 }, 1000);
